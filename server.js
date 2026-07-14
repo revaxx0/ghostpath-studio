7ea9437eb318f721e5a30d03a4e2383b3f4e074d
revaxx0 <sonmeznecmi60@gmail.com>
2026-07-14 14:34:53 +0300

    Add new game: Spectra (placeholder, uses GhostPath.jpg)

--- server.js
+++ server.js
@@ -71,6 +71,27 @@ const games = [
     statusTR: 'Çok Yakında',
     statusJP: '近日公開',
     statusRU: 'Скоро'
+  },
+  {
+    id: 'spectra',
+    title: 'Spectra',
+    tag: 'Horror',
+    descEN: 'Coming soon.',
+    descTR: 'Çok yakında.',
+    descJP: '近日公開。',
+    descRU: 'Скоро.',
+    fullDescEN: 'Coming soon.',
+    fullDescTR: 'Çok yakında.',
+    fullDescJP: '近日公開。',
+    fullDescRU: 'Скоро.',
+    releaseDate: 'TBA',
+    image: '/images/GhostPath.jpg',
+    gallery: ['/images/GhostPath.jpg'],
+    trailer: 'https://www.youtube.com/watch?v=',
+    statusEN: 'Coming Soon',
+    statusTR: 'Çok Yakında',
+    statusJP: '近日公開',
+    statusRU: 'Скоро'
   }
 ];
 
